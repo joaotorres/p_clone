dca84bef032c28969b2a59da13c59bbc1192e723
Joao Torres <joaoatorres@gmail.com>
2018-12-07 08:35:45 -0200

    new commit

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class ProductsTest < ApplicationSystemTestCase
   test "visiting the index" do
-    visit "/"
+    visit "/" #
     assert_selector "h1", text: "Awesome Products"
     assert_selector ".product", count: Product.count
   end
